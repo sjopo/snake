6d93b16c265f371d587a046283b89b1e80ababd1
sjopo <167049628+sjopo@users.noreply.github.com>
2024-04-14 20:24:16 -0700

    Add files via upload

---
+++ snake.js
@@ -0,0 +1,117 @@
+﻿// board
+var blockSize = 25;
+var rows = 20;
+var cols = 20;
+var board, context;
+
+
+var velocityX = 0;
+var velocityY = 0;
+
+var snakeBody = []
+
+// headd
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+
+// apples
+var foodX;
+var foodY;
+
+var gameover = false;
+
+window.onload = function () {
+    board = document.getElementById("board");
+    board.height = (rows * blockSize);
+    board.width = (cols * blockSize);
+    context = board.getContext("2d");
+
+
+    placefoodd5();
+
+
+    document.addEventListener("keyup", changeDirection);
+
+
+    setInterval(update, 1000 / 10);
+
+
+    //update();
+};
+
+
+function update() {
+    if (gameover) {
+        return;
+    }
+
+    context.fillStyle = "black";
+    context.fillRect(0, 0, board.width, board.height);
+
+    context.fillStyle = "red";
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+
+
+    if (snakeX == foodX && snakeY == foodY) {
+        snakeBody.push([foodX, foodY])
+        placefood();
+    }
+
+    for (let i = snakeBody.length - 1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i - 1];
+    }
+
+    if (snakeBody.length) {
+        snakeBody[0] = [snakeX, snakeY];
+    }
+
+    context.fillStyle = "lime";
+    snakeX += velocityX * blockSize;
+    snakeY += velocityY * blockSize;
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+    for (let i = 0; i < snakeBody.length; i++) {
+        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+
+        if (snakeX < 0 || snakeX > cols * blockSize || snakeY < 0 || snakeY > rows * blockSize) {
+            gameover = true;
+            alert("game over");
+
+        }
+        for (let i = 0; i < snakeBody.length; i++) {
+            if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
+                gameover = true;
+                alert("game over");
+            }
+        }
+    }
+}
+
+    function changeDirection(e) {
+        if (e.code == "ArrowUp" && velocityY != 1) {
+            velocityX = 0;
+            velocityY = -1;
+        }
+        else if (e.code == "ArrowDown" && velocityY != -1) {
+            velocityX = 0;
+            velocityY = 1;
+        }
+        else if (e.code == "ArrowLeft" && velocityX != 1) {
+            velocityX = -1;
+            velocityY = 0;
+        }
+        else if (e.code == "ArrowRight" && velocityX != -1) {
+            velocityX = 1;
+            velocityY = 0;
+        }
+    }
+
+    function placefood() {
+        (foodX = Math.floor(Math.random() * cols) * blockSize);
+        (foodY = Math.floor(Math.random() * rows) * blockSize);
+    }
+
+    function placefoodd5() {
+        placefood()
+
+    }
